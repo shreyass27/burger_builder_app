7ed36c30b21e08c355c2be3b7290f742529dccad
Shreyas Shetty <shreyass2709@gmail.com>
2018-12-04 14:38:49 +0530

    Added Modal

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import classes from './Modal.scss';
+import Backdrop from '../Backdrop/Backdrop';
+
 
 const modal = (props) => (
-    <div className={classes.Modal} 
-        style={{ transform: props.show ? 'translateY(0)' : 'translateY(100vh)' }}>
-        { props.children }
-    </div>
+    <Fragment>
+        <Backdrop click={props.onModalClose} show={props.show} />
+        <div className={classes.Modal} 
+            style={{ transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
+                    opacity: props.show ? '1' : '0' }}>
+            { props.children }
+        </div>
+    </Fragment>
 )
 
 export default modal;
