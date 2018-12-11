e864cc9caa8698dbbd28f4eb4f14bf124b78aba3
Shreyas Shetty <shreyass2709@gmail.com>
2018-12-11 19:08:50 +0530

    Order Action and Reducer Changes

--- src/store/actions/order.js
+++ src/store/actions/order.js
@@ -1,1 +1,34 @@
-// import * as actionTypes from './actionTypes';
+import * as actionTypes from './actionTypes';
+import orderAxios from '../../axiosOrders';
+
+const purchaseBurgerSuccess = (orderId, orderData) => ({
+    type: actionTypes.PURCHASE_BURGER_SUCCESS,
+    orderId,
+    orderData
+});
+
+const purchaseBurgerFailure = (error) => ({
+    type: actionTypes.PURCHASE_BURGER_FAILED,
+    error
+});
+
+export const purchaseBurgerStart = () => ({
+    type: actionTypes.PURCHASE_BURGER_START
+});
+
+export const purchaseInit = () => ({
+    type: actionTypes.PURCHASE_INIT
+});
+
+export const purchaseBurger = (orderData) => {
+    return (dispatch) => {
+        dispatch(purchaseBurgerStart());
+        orderAxios.post('orders.json', orderData )
+            .then( response => {
+                dispatch(purchaseBurgerSuccess(response.data.name, orderData))
+            })
+            .catch( error => {
+                dispatch(purchaseBurgerFailure(error.message))
+            });
+    }
+}
---
+++ src/store/reducers/order.js
@@ -0,0 +1,37 @@
+import * as actionTypes from '../actions/actionTypes';
+import { updateState } from '../utility';
+
+const initialState = {
+    orders: [],
+    loading: false,
+    isPurchasing: false
+}
+
+const addOrder = (state, action) => {
+    const newOrder = {
+        ...action.orderData,
+        id: action.orderId
+    }
+    return updateState(state, {
+        orders: state.orders.concat(newOrder),
+        loading: false,
+        isPurchasing: false
+    });
+}
+
+const orderReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case actionTypes.PURCHASE_INIT:
+            return updateState(state, { isPurchasing: true });
+        case actionTypes.PURCHASE_BURGER_START:
+            return updateState(state, { loading: true });
+        case actionTypes.PURCHASE_BURGER_SUCCESS:
+            return addOrder(state, action);
+        case actionTypes.PURCHASE_BURGER_FAILED:
+            return updateState(state, { loading: false });
+        default:
+            return state;
+    }
+}
+
+export default orderReducer;
