9008fcce0bc6c98201b57d08f23c9d8ebbad8268
Shreyas Shetty <shreyass2709@gmail.com>
2018-12-12 16:56:34 +0530

    App.js Refactoring and AppRoutes.js Implementation

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
 import React, { Component } from 'react';
-import { Route, Switch } from 'react-router-dom';
 import Layout from './containers/Layout/Layout';
-import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
-import Checkout from './containers/Checkout/Checkout';
-import Orders from './containers/Orders/Orders';
-import Auth from './containers/Auth/Auth';
-
+import AppRoutes from './AppRoutes';
 class App extends Component {
   render() {
     return (
       <div>
           <Layout>
-            <Switch>
-              <Route path="/checkout" component={Checkout} />
-              <Route path="/orders" component={Orders} />
-              <Route path="/auth" component={Auth} />
-              <Route path="/" component={BurgerBuilder} />
-            </Switch>
+            <AppRoutes />
           </Layout>
       </div>
     );
---
+++ src/AppRoutes.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
+import Checkout from './containers/Checkout/Checkout';
+import Orders from './containers/Orders/Orders';
+import Auth from './containers/Auth/Auth';
+import Logout from './containers/Auth/Logout/Logout';
+
+const appRoutes = () => (
+    <Switch>
+        <Route path="/checkout" component={Checkout} />
+        <Route path="/orders" component={Orders} />
+        <Route path="/auth" component={Auth} />
+        <Route path="/logout" component={Logout} />
+        <Route path="/" component={BurgerBuilder} />
+  </Switch>
+);
+
+export default appRoutes;
