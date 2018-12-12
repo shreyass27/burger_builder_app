e4f9a0898894e873ae2e3eec348ffe0510f2d582
Shreyas Shetty <shreyass2709@gmail.com>
2018-12-12 16:00:16 +0530

    Auth State Root Reducer Config

--- src/store/configStore.js
+++ src/store/configStore.js
@@ -4,13 +4,15 @@ import thunk from 'redux-thunk';
 
 import ingredientReducer from './reducers/ingredients';
 import orderReducer from './reducers/order';
+import authReducer from './reducers/auth';
 
 export default () => {
     const compomseEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
     const rootReducer = combineReducers({
         ingredient: ingredientReducer,
-        order: orderReducer
+        order: orderReducer,
+        auth: authReducer
     });
 
     return createStore(rootReducer, compomseEnhancers(applyMiddleware(thunk)));
